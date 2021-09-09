68224646ec10d9da6cffa6e2ed605fde26d3e932
Fer-Rodriguez <ref2k1@gmail.com>
2021-09-09 04:52:56 -0500

    preview game sequence

---
+++ app/src/main/java/com/example/coquille/preview_sequence.kt
@@ -0,0 +1,11 @@
+package com.example.coquille
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class preview_sequence : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview_sequence)
+    }
+}
