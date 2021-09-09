f92af0a9af2be885fdc072d6dce281a782faa44b
vilkas99 <victorium99@hotmail.com>
2021-09-09 00:58:07 -0500

    profile done

--- app/src/main/java/com/example/coquille/profile.kt
+++ app/src/main/java/com/example/coquille/profile.kt
@@ -2,10 +2,34 @@ package com.example.coquille
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class profile : AppCompatActivity() {
+
+    val infoFragment = ProfileInfo_Fragment()
+    val achivementFragment = ProfileAchivement_Fragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.profile_fragment, infoFragment)
+                commit()
+            }
+    }
+
+    fun changeToInfo(view: View) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.profile_fragment, infoFragment)
+            commit()
+        }
+    }
+
+    fun changeToAchivement(view: View) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.profile_fragment, achivementFragment)
+            commit()
+        }
     }
 }
