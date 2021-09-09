33ffbe263a3e3d7b3995b3f88420361ff3b46914
Uziel Humberto López Meneses <a01733922@itesm.mx>
2021-09-09 09:52:21 -0500

    Deleted unecessary code

--- app/src/main/java/com/example/coquille/MainActivity.kt
+++ app/src/main/java/com/example/coquille/MainActivity.kt
@@ -9,11 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-
-
-    fun goToSettings(view: View){
-        intent = Intent(this, Settings::class.java)
-        startActivity(intent)
-    }
+    
 }
