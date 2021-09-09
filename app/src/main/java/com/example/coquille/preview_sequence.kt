224f976d83ba66221c6f96e1d708313ad10c5f35
vilkas99 <victorium99@hotmail.com>
2021-09-09 10:21:20 -0500

    Routing between preview & sequence DONE

--- app/src/main/java/com/example/coquille/preview_sequence.kt
+++ app/src/main/java/com/example/coquille/preview_sequence.kt
@@ -1,11 +1,19 @@
 package com.example.coquille
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class preview_sequence : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview_sequence)
+    }
+
+    fun routeToSequenceGame(view: View) {
+        val intent = Intent(this, Sequence_game::class.java)
+        startActivity(intent)
     }
 }
