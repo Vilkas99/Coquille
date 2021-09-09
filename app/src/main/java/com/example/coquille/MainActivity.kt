dc503ef5b9b9c2432ef08afb0fed6ae3ae0f975f
Uziel Humberto López Meneses <a01733922@itesm.mx>
2021-09-09 09:47:08 -0500

    Base for settings UI

--- app/src/main/java/com/example/coquille/MainActivity.kt
+++ app/src/main/java/com/example/coquille/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.coquille
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+    fun goToSettings(view: View){
+        intent = Intent(this, Settings::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/coquille/Settings.kt
@@ -0,0 +1,15 @@
+package com.example.coquille
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Settings : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+
+
+
+}
