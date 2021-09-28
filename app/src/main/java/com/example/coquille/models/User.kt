83fb4c3929d2f46f89393ac2985fa30e46edc8b1
vilkas99 <victorium99@hotmail.com>
2021-09-28 16:40:12 -0500

    Add settings to User model

--- app/src/main/java/com/example/coquille/models/User.kt
+++ app/src/main/java/com/example/coquille/models/User.kt
@@ -1,9 +1,10 @@
 package com.example.coquille.models
 
-class User constructor(userName: String, password: String, profilePic: String, points: Int, collectables:Array<Collectable>) {
+class User constructor(userName: String, password: String, profilePic: String, points: Int, collectables:Array<Collectable>, settings: Settings) {
     val userName = userName
     val password = password
     val profilePic = profilePic
     val points = points
     val collectables = collectables
+    val settings = settings
 }
