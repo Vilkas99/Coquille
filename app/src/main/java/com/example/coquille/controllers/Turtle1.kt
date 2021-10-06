9e2a641ad887bc94325ceeda3de6815de7f4e322
vilkas99 <victorium99@hotmail.com>
2021-10-05 19:03:06 -0500

    UI almost done

---
+++ app/src/main/java/com/example/coquille/controllers/Turtle1.kt
@@ -0,0 +1,11 @@
+package com.example.coquille
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Turtle1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_turtle1)
+    }
+}
